5bd8b179b36a3d7511b2ce17fb909a0ca3720810
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-13 18:15:00 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -entry point of a program where the execution of a program starts
+ *
+ * Return: Always end the execution of a function(0)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
