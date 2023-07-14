5404ec66c330ef897ea46c138be2bc0e27509f33
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-13 19:41:04 -0700

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unist.h>
+/**
+ * main - you have to be a genius to understand the simplicity of unix
+ *
+ * Return : alway return (1)
+ *
+ */
+int main(void)
+{
+	write("and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
