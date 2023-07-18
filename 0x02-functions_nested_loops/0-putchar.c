810d70086299422ffc0238d98aeb2b8b8cb356f1
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-18 13:23:07 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main-writes the character c to stdout
+ * Description:The character to print
+ *
+ * Return: On success 0.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int main(void)
+{
+
+	printf("_putchar\n");
+	return (0);
+}
