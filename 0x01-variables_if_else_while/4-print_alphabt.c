7b0b5cca335de28df8960320ba855e24fb2b9da1
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-17 18:16:18 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main-Using the continue variable for skip
+ *
+ * Return:Always return (0)
+ *
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		if (alpha == 'e' || alpha == 'q')
+		{
+			putchar (alpha);
+		}
+	{
+		putchar ('\n')
+	}
+	return (0)
+}
