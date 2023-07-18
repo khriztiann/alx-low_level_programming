74c2d32c7ebcb12752d7d8ae2148089e8e3bcc6e
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-18 13:35:36 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+int _putchar(char c);
+void print_alphabets(void);
+void print_alphabets_x10(void);
+void print_to_99(int n);
+void print_times_table(int n);
+int add(int, int);
+void print_last_digit(int);
+int _abs(int);
+int _islower(int c);
+int _isalpha(int);
+int print_sign(int n);
