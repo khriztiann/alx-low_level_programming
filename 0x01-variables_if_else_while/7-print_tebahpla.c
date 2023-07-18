fd7c07b33955fa665d7c1f56b53d968a1ddef427
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-17 19:44:26 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main-Reverse alphabet
+ *
+ * Return:Always (0)
+ *
+ */
+int main(void)
+{
+	int rev_alpha;
+
+	for (rev_alpha = 'z'; rev_alpha <= 'z'; rev_alpha--)
+	{
+		putchar (rev_alpha);
+	}
+	{
+		putchar ('\n');
+	}
+	return (0);
+}
