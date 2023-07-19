f1172167b0406e4c453b9d3834758226a9bbfb31
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-18 18:55:15 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - There is nothing like absolute value
+ * @c:  a function that computes the absolute value of an integer
+ * Return: return 0
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val = c * -1;
+		return (abs_val);
+	}
+	return (c);
+}
