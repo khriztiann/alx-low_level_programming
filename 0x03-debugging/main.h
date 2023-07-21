bb155ef3cfea80136db533d069644d4c0cc12a92
khriztiann <www.khriztiannpadingah@gmail.com>
2023-07-21 15:37:17 -0700

    Task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,10 @@
 int largest_number(int a, int b, int c);
 int largest_number(int a, int b, int c);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
